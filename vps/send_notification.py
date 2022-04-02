43c3f2797e984875c16bcb1cf5d733ed880044c6
ntdgy <ntdgy2001@gmail.com>
2022-04-02 14:56:48 +0800

    4.2

--- vps/send_notification.py
+++ vps/send_notification.py
@@ -2,10 +2,18 @@ import requests
 
 
 def sendMessage(message):
-    #url = "https://tg.cf.sustc.icu/bot5110496404:AAFpJmyuBZPMTHZZU01GooDILT_3ghutkCQ/sendMessage?chat_id=-1001740652895&text="
-    url = "https://tg.cf.sustc.icu/bot5110496404:AAFpJmyuBZPMTHZZU01GooDILT_3ghutkCQ/sendMessage?chat_id=-1001620302112&text="
-    r = requests.post(url + message)
-    print(r.text)
+    url = "https://tg.cf.sustc.icu/bot5110496404:AAFpJmyuBZPMTHZZU01GooDILT_3ghutkCQ/sendMessage"
+    data = {'chat_id': '-1001620302112', 'text': message}
+    r = requests.post(url,data)
+    #print(r.text)
+    return r.text
+
+def sendDocument(file_name):
+    url = "https://tg.cf.sustc.icu/bot5110496404:AAFpJmyuBZPMTHZZU01GooDILT_3ghutkCQ/sendDocument"
+    data = {'chat_id': '-1001620302112', 'document': open(file_name, 'rb')}
+    r = requests.post(url,data)
+    return r.text
+
 
 def main():
     f = open('/root/bandwidth/bandwidth.txt')
@@ -18,4 +26,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    sendMessage('test')
+    print(sendDocument('file.db'))
